ecf3053b6923260c013f9503c06d219d4e802e7d
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-08 13:22:29 +0300

    part 3  - Create 1 method

--- src/ua/javarush/task/task39/task3913/Utils/DataQueryImpl.java
+++ src/ua/javarush/task/task39/task3913/Utils/DataQueryImpl.java
@@ -1,11 +1,13 @@
 package ua.javarush.task.task39.task3913.Utils;
 
 import ua.javarush.task.task39.task3913.DAO.LogReader;
+import ua.javarush.task.task39.task3913.DTO.LogEntry;
 import ua.javarush.task.task39.task3913.Event;
 
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DataQueryImpl {
     private final LogReader logReader;
@@ -15,7 +17,11 @@ public class DataQueryImpl {
     }
 
     public Set<Date> getDatesForUserAndEvent(String user, Event event, Date after, Date before) {
-        return null;
+        return logReader.getLogs().stream()
+                .filter(logEntry -> isDateInRange(logEntry.getDate(), after, before))
+                .filter(logEntry -> logEntry.getUser().equalsIgnoreCase(user) && logEntry.getEvent().equals(event))
+                .map(LogEntry::getDate)
+                .collect(Collectors.toSet());
     }
 
     public Set<Date> getDatesWhenSomethingFailed(Date after, Date before) {
