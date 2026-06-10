8694060d41275b64f232e58b0417fa21e4895d74
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-10 13:46:08 +0300

    part 5 - refactor structure

--- src/ua/javarush/task/task39/task3913/Utils/QLQ/QLQueryReader.java
+++ src/ua/javarush/task/task39/task3913/Utils/QLQ/QLQueryReader.java
@@ -1,4 +1,31 @@
 package ua.javarush.task.task39.task3913.Utils.QLQ;
 
+import ua.javarush.task.task39.task3913.DTO.QueryEntry;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class QLQueryReader {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static QueryEntry readQuery() {
+        while (true) {
+            System.out.println("Enter Query:");
+
+            try {
+                String line = reader.readLine();
+                if (line.equals("")) {
+
+                    return parseQuery(line);
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Query, please try again");
+                System.out.println();
+            }
+        }
+    }
+
+    private static QueryEntry parseQuery(String line) {
+        return null;
+    }
 }
