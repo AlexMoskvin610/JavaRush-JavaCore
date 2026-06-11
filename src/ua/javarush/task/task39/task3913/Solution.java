9a660d2107fe31f9f046c37dd77f9a08daa95a46
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-11 17:16:45 +0300

    part 6 - reformat5

--- src/ua/javarush/task/task39/task3913/Solution.java
+++ src/ua/javarush/task/task39/task3913/Solution.java
@@ -5,11 +5,13 @@ import java.nio.file.Paths;
 public class Solution {
     private static final String LOG_FILE_PATH = "src/ua/javarush/task/task39/task3913/logs/";
     private static final String TYPE2_QUERY = "get ip for user = \"Vasya\"";
+    private static final String TYPE2_QUERY2 = "get user for ip = \"192.168.100.2\"";
 
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get(LOG_FILE_PATH));
 
-        logParser.execute(TYPE2_QUERY);
+        System.out.println(logParser.getUsersForIP("192.168.100.2", null, null));
+        System.out.println(logParser.execute(TYPE2_QUERY2));
 
     }
 }
