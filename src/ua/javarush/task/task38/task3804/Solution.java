723dd2f7363a913a264ca7b62f2a38bc3e1c4e52
Oleksandr Moskvin <moskvin_oleksandr@proton.me>
2026-06-13 17:45:28 +0300

    start task - hierarchy of exceptions

---
+++ src/ua/javarush/task/task38/task3804/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.task.task38.task3804;
+
+/* 
+Фабрика винятків
+*/
+
+public class Solution {
+    public static Class getFactoryClass() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
