ece4d78afe26305703cb40f6ea3b3f36a3ea2788
Oleksandr Moskvin <moskvin_oleksandr@proton.me>
2026-06-13 19:29:36 +0300

    start task - hierarchy of exceptions

--- src/ua/javarush/task/task38/task3804/Solution.java
+++ src/ua/javarush/task/task38/task3804/Solution.java
@@ -4,12 +4,27 @@ package ua.javarush.task.task38.task3804;
 Фабрика винятків
 */
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Solution {
     public static Class getFactoryClass() {
-        return null;
+        return new ExceptionFactory().getClass();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+       String fullClassName = getFactoryClass().getName(); // "com.example.MyFactory"
 
+        try {
+            // 1. Загружаем класс по полной строке
+            Class<?> loadedClass = Class.forName(fullClassName);
+            // 2. Находим статический метод
+            Method method = loadedClass.getMethod("getException", Enum.class);
+            Throwable resultException = (Throwable) method.invoke(null, ApplicationExceptionMessage.UNHANDLED_EXCEPTION);
+            // Теперь у тебя в руках готовый Throwable объект, созданный динамически!
+            System.out.println("Исключение успешно создано: " + resultException.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
