8d7c1235c98efd254ce43b6b7472b8da1b5c7303
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-13 20:13:29 +0300

    task 3805

--- src/ua/javarush/task/task38/task3804/ExceptionFactory.java
+++ src/ua/javarush/task/task38/task3804/ExceptionFactory.java
@@ -2,11 +2,21 @@ package ua.javarush.task.task38.task3804;
 
 public class ExceptionFactory {
     public static Throwable getException(Enum enumValue) {
-        if (enumValue instanceof ApplicationExceptionMessage) {
-            return new Exception(enumValue.toString().replaceAll("_", " "));
-        } else if (enumValue instanceof UserExceptionMessage) {
-            return new RuntimeException(enumValue.toString().replaceAll("_", " "));
+        if (enumValue == null) {
+            return new IllegalArgumentException();
         }
+
+        String message = enumValue.toString().replace("_", " ").trim().toLowerCase();
+        String formattedMessage = message.substring(0, 1).toUpperCase() + message.substring(1);
+
+        if (enumValue instanceof ApplicationExceptionMessage) {
+            return new Exception(formattedMessage);
+        } else if (enumValue instanceof UserExceptionMessage) {
+            return new Error(formattedMessage);
+        }else if (enumValue instanceof DatabaseExceptionMessage){
+            return new RuntimeException(formattedMessage);
+        }
+
         return new IllegalArgumentException();
     }
 }
