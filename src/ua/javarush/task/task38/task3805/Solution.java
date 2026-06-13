cc70fc4f4ce768f63f5df004356dbf31b4dd35d7
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-13 20:23:24 +0300

    task 3805 - solved

--- src/ua/javarush/task/task38/task3805/Solution.java
+++ src/ua/javarush/task/task38/task3805/Solution.java
@@ -11,44 +11,35 @@ public class Solution {
         try {
             connection = new ConnectionMock();
             connection.connect();
-        } catch (WrongDataException e) {
+        } catch (WrongDataException | ConnectionException e) {
             throw new SolutionException("WrongDataException: " + e.getMessage());
-        } catch (ConnectionException e) {
-            throw new SolutionException("ConnectionException: " + e.getMessage());
         }
     }
 
     public void write(Object data) throws SolutionException {
         try {
             connection.write(data);
-        } catch (WrongDataException e) {
+        } catch (WrongDataException | ConnectionException e) {
             throw new SolutionException("WrongDataException: " + e.getMessage());
-        } catch (ConnectionException e) {
-            throw new SolutionException("ConnectionException: " + e.getMessage());
         }
     }
 
     public Object read() throws SolutionException {
         try {
             return connection.read();
-        } catch (WrongDataException e) {
+        } catch (WrongDataException | ConnectionException e) {
             throw new SolutionException("WrongDataException: " + e.getMessage());
-        } catch (ConnectionException e) {
-            throw new SolutionException("ConnectionException: " + e.getMessage());
         }
     }
 
     public void disconnect() throws SolutionException {
         try {
             connection.disconnect();
-        } catch (WrongDataException e) {
+        } catch (WrongDataException | ConnectionException e) {
             throw new SolutionException("WrongDataException: " + e.getMessage());
-        } catch (ConnectionException e) {
-            throw new SolutionException("ConnectionException: " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-
     }
 }
