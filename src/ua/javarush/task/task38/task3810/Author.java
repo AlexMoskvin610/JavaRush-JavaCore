c6bc3ec42cd68f61053569aebec0f3d59b57600c
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-18 16:04:44 +0300

    task 3811 - solved

---
+++ src/ua/javarush/task/task38/task3810/Author.java
@@ -0,0 +1,6 @@
+package ua.javarush.task.task38.task3810;
+
+public @interface Author {
+    String value();
+    Position position() default Position.OTHER;
+}
---
+++ src/ua/javarush/task/task38/task3811/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.task38.task3811;
+
+/* 
+Тікети
+*/
+
+@Ticket(
+        priority = Ticket.Priority.HIGH,
+        createdBy = "Noodles",
+        tags = {"bug", "fix asap"}
+)
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
