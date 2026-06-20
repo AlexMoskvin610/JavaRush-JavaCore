6df081614acb75329a58eb10071f2ce3fbf1bd4f
Oleksandr Moskvin <moskvin_oleksandr@proton.me>
2026-06-20 10:00:45 +0300

    task2810-part3 - solved

---
+++ src/ua/javarush/task/task28/task2810/model/LinkedinStrategy.java
@@ -0,0 +1,4 @@
+package ua.javarush.task.task28.task2810.model;
+
+public class LinkedinStrategy implements Strategy {
+}
--- src/ua/javarush/task/task28/task2810/model/Provider.java
+++ src/ua/javarush/task/task28/task2810/model/Provider.java
@@ -1,5 +1,10 @@
 package ua.javarush.task.task28.task2810.model;
 
+import ua.javarush.task.task28.task2810.vo.JobPosting;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Provider {
     private Strategy strategy;
 
@@ -9,5 +14,9 @@ public class Provider {
 
     public Provider(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    public List<JobPosting> getJavaJobPostings(String searchString) {
+        return new ArrayList<>();
     }
 }
