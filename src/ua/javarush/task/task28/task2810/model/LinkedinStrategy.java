f66929a8b649c59a3e96f4a1ade9a1d12665d0a3
Oleksandr Moskvin <moskvin_oleksandr@proton.me>
2026-06-20 10:40:11 +0300

    task2810-part4 - solved

--- src/ua/javarush/task/task28/task2810/model/LinkedinStrategy.java
+++ src/ua/javarush/task/task28/task2810/model/LinkedinStrategy.java
@@ -1,4 +1,9 @@
 package ua.javarush.task.task28.task2810.model;
 
 public class LinkedinStrategy implements Strategy {
+    private static final String URL_FORMAT = "https://www.linkedin.com/jobs/search?keywords=Java+%s&start=%d";
+
+    public String getURL_FORMAT() {
+        return URL_FORMAT;
+    }
 }
