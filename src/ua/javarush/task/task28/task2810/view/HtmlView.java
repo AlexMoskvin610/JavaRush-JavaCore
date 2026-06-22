34786f3e80b857cecf297af3523e89a64df4957a
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-22 10:22:24 +0300

    task2810 - part 15 - create getDocument method

--- src/ua/javarush/task/task28/task2810/view/HtmlView.java
+++ src/ua/javarush/task/task28/task2810/view/HtmlView.java
@@ -1,13 +1,18 @@
 package ua.javarush.task.task28.task2810.view;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import ua.javarush.task.task28.task2810.Controller;
 import ua.javarush.task.task28.task2810.vo.JobPosting;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
-    private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replaceAll("[.]", "/") + "/jobPostings.html";
+    // private final String filePath = "./4.JavaCollections/src/" + this.getClass().getPackage().getName().replaceAll("[.]", "/") + "/jobPostings.html";
+    private final String filePath = "src/ua/javarush/task/task28/task2810/view/jobPostingsTest.html";
 
     private Controller controller;
 
@@ -43,5 +48,9 @@ public class HtmlView implements View {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
