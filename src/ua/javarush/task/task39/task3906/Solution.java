3ae7d4548c642636eafd470f687d348f572028b7
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-26 15:41:01 +0300

    task 39 - start solving

--- src/ua/javarush/task/task39/task3906/Solution.java
+++ src/ua/javarush/task/task39/task3906/Solution.java
@@ -7,8 +7,12 @@ package ua.javarush.task.task39.task3906;
 public class Solution {
     public static void main(String[] args) {
         ElectricPowerSwitch electricPowerSwitch = new ElectricPowerSwitch(new SecuritySystem());
+        ElectricPowerSwitch electricPowerSwitch2 = new ElectricPowerSwitch(new LightBulb());
 
         electricPowerSwitch.press();
         electricPowerSwitch.press();
+
+        electricPowerSwitch2.press();
+        electricPowerSwitch2.press();
     }
 }
