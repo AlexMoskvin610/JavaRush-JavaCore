4359730f967d27ddec97d71f69d818efa1b0eed4
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-27 11:36:12 +0300

    task 3912 - started

---
+++ src/ua/javarush/task/task39/task3912/Solution.java
@@ -0,0 +1,15 @@
+package ua.javarush.task.task39.task3912;
+
+/* 
+Максимальна площа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxSquare(int[][] matrix) {
+
+    }
+}
