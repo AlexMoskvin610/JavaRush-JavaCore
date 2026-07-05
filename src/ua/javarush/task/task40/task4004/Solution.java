3b07e8638f6d805ce691a778c9238928189ac622
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-07-05 13:00:12 +0300

    task 4004 - pre solved

--- src/ua/javarush/task/task40/task4004/Solution.java
+++ src/ua/javarush/task/task40/task4004/Solution.java
@@ -20,18 +20,27 @@ class Point {
 public class Solution {
     public static void main(String[] args) {
         List<Point> polygon = new ArrayList<>();
+
         polygon.add(new Point(0, 0));
         polygon.add(new Point(0, 10));
         polygon.add(new Point(10, 10));
         polygon.add(new Point(10, 0));
 
         System.out.println(isPointInPolygon(new Point(5, 5), polygon));
+        System.out.println(isPointInPolygon(new Point(10, 5), polygon));
+        System.out.println(isPointInPolygon(new Point(2, 8), polygon));
+        System.out.println(isPointInPolygon(new Point(11, 10), polygon));
         System.out.println(isPointInPolygon(new Point(100, 100), polygon));
     }
 
     public static boolean isPointInPolygon(Point point, List<Point> polygon) {
-        //напишіть тут ваш код
-    }
+        int maxX = polygon.stream().mapToInt(p -> p.x).max().orElse(0);
+        int minX = polygon.stream().mapToInt(p -> p.x).min().orElse(0);
 
+        int maxY = polygon.stream().mapToInt(p -> p.y).max().orElse(0);
+        int minY = polygon.stream().mapToInt(p -> p.y).min().orElse(0);
+
+        return point.x >= minX && point.x <= maxX  && point.y >= minY && point.y <= maxY;
+    }
 }
 
