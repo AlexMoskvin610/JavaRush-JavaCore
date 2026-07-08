363f154780e3b40cc54b48536a25ea6878491e13
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-07-08 11:05:27 +0300

    Task 3410-3  solved

--- src/ua/javarush/task/task34/task3410/controller/Controller.java
+++ src/ua/javarush/task/task34/task3410/controller/Controller.java
@@ -10,8 +10,10 @@ public class Controller {
     public Controller() {
         this.model = new Model();
         this.view = new View(this);
+        this.view.init();
     }
 
     public static void main(String[] args) {
+        Controller controller = new Controller();
     }
 }
