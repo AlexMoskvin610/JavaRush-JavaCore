34afb20af7c148b1e240b1863323afd3ccf4345c
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-07-09 14:20:44 +0300

    Task 3410-15 created checkWallCollision

--- src/ua/javarush/task/task34/task3410/model/Model.java
+++ src/ua/javarush/task/task34/task3410/model/Model.java
@@ -3,6 +3,7 @@ package ua.javarush.task.task34.task3410.model;
 import ua.javarush.task.task34.task3410.controller.EventListener;
 
 import java.nio.file.Paths;
+import java.util.Set;
 
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;
@@ -39,5 +40,16 @@ public class Model {
     }
 
     public void move(Direction direction) {
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+       Set<Wall> walls = gameObjects.getWalls();
+        for (Wall wall : walls) {
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
